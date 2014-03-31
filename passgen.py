984808579cefb19276ab0640850152e840e027af
Mark Nichols <mark@zanshin.net>
2014-03-30 21:08:29 -0500

    python password generation script

---
+++ passgen.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import random
+f = open('/usr/share/dict/words')
+words = map(lambda x: x.strip(), f.readlines())
+password = '-'.join(random.choice(words) for i in range(2)).capitalize()
+password += str(random.randint(1, 9999))
+print password
