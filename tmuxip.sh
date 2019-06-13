03e7c62c55ee177de7486b69c41827d6c25a6f3f
Mark Nichols <mark@zanshin.net>
2019-06-13 14:02:21 -0500

    New script to display VPN ip address

--- tmuxip.sh
+++ tmuxip.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-PUBIP=`curl -s https://api.ipify.org | awk '{print $1}'`
-if [[ `uname -a | grep -i darwin` ]]; then
-  OTHERIPS=`netstat -i -n | egrep "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v lo | awk '{print " : " $1":"$4}'`
-else
-  OTHERIPS=`for i in `netstat -i | egrep -v "^(Iface|lo|Ker)" | awk '{print $1}'` ; do echo -ne $i ; ip -4 addr show $i | grep inet | awk '{print " : " $2}' ; done`
-fi
-echo $PUBIP $OTHERIPS
+UTUNIP=`(ifconfig | grep "inet 10.130" | grep -v broadcast | awk '{print $2}')`
+echo " : " $UTUNIP
