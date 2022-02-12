cf0537711610151e514f5f52ec95b9b1ddcf5446
Mark Nichols <mark@zanshin.net>
2022-02-12 10:53:23 -0600

    tmux sessionizer

--- bash/tsession.sh
+++ bash/tsession.sh
@@ -9,7 +9,7 @@ if [[ $# -eq 1 ]]; then
     selected=$1
 else
     # selected=$(find ~/work/builds ~/ ~/work ~/personal -mindepth 1 -maxdepth 1 -type d | fzf)
-    selected=$(find ~/code ~/ -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/code ~/ ~/src -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z "$selected" ]]; then
